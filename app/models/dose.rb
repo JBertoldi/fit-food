a4d5df1daefdf1d2e678534f3fbafdddfe2159f4
Joana Bertoldi <joanasoares_1994@msn.com>
2020-12-03 21:12:24 +0000

    Add validations and associations

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,7 @@
 class Dose < ApplicationRecord
   belongs_to :recipe
   belongs_to :ingredient
+
+  validates :amount, numericality: { greater_than: 0, message: 'Please enter a number greater than 0.' }
+  validates :description, length: { maximum: 50, message: 'Please write a description with up to 50 characters' }
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,8 @@
 class Recipe < ApplicationRecord
+  has_many :ingredients, through: :doses
+  has_many :doses, dependent: :destroy
+
+  validates :name, format: { with: /\A([a-zA-Z0-9])+([\s][a-zA-Z0-9]+)*\Z/, message: "The recipe's name may only contain letters, numbers and single spaces." }
+  validates :name, length: { in: 5..50, message: 'Please enter a name with a minimum of 5, and a maximum of 50 characters.' }
+  validates :preparation, length: { minimum: 10, message: 'Please write instructions with a minimum of 10 characters.' }
 end
