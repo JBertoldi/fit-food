1de73aae5c992d940f14ff0242ff4379fa3bc4a0
Joana Bertoldi <joanasoares_1994@msn.com>
2020-12-04 08:50:31 +0000

    Add ingredients routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'ingredients/index'
-  get 'ingredients/new'
-  get 'ingredients/create'
   resources :recipes, except: %i[edit update destroy]
+
+  resources :ingredients, only: %i[index new create]
 end
