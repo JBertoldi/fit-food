14d66f39b8cd64879a4db493e30b85c66a64719f
Joana Bertoldi <joanasoares_1994@msn.com>
2020-12-05 15:04:53 +0000

    Add navbar template and home banner image

---
+++ app/javascript/components/navbar.js
@@ -0,0 +1,10 @@
+navbar = () => {
+  $('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+  });
+}
+
+export { navbar };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,9 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import { navbar } from "components/navbar";
+
+document.addEventListener('turbolinks:load', () => {
+  navbar();
+});
