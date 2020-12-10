8f503369b2a635173370c4e983e8939bbfad5564
Joana Bertoldi <joanasoares_1994@msn.com>
2020-12-10 22:58:07 +0000

    Add method to return recipe's total macronutrients

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,9 +1,21 @@
 class DosesController < ApplicationController
   before_action :set_recipe
+  before_action :set_recipe_macros, only: :new
 
   def new
-    @new_recipe = Recipe.new
     @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.amount /= 100
+    @dose.recipe = @recipe
+
+    if @dose.save
+      redirect_to new_recipe_dose_path(@recipe)
+    else
+      render :new
+    end
   end
 
   def update
@@ -13,17 +25,6 @@ class DosesController < ApplicationController
       redirect_to new_recipe_dose_path(@recipe)
     else
       redirect_to new_recipe_dose_path(@recipe), notice: 'OH NÃOOOOOOOOOOOO :O'
-    end
-  end
-
-  def create
-    @dose = Dose.new(dose_params)
-    @dose.recipe = @recipe
-
-    if @dose.save
-      redirect_to new_recipe_dose_path(@recipe)
-    else
-      render :new
     end
   end
 
@@ -47,4 +48,23 @@ class DosesController < ApplicationController
   def set_recipe
     @recipe = Recipe.last
   end
+
+  def set_recipe_macros
+    @recipe_kcal = 0
+    @recipe_carbs = 0
+    @recipe_total_fats = 0
+    @recipe_sat_fats = 0
+    @recipe_prot = 0
+    @recipe_salt = 0
+
+    @recipe.doses.each do |dose|
+      @recipe_kcal += dose.ingredient.kcal * dose.amount
+      @recipe_carbs += dose.ingredient.carbs * dose.amount
+      @recipe_total_fats += dose.ingredient.total_fats * dose.amount
+      @recipe_sat_fats += dose.ingredient.saturated_fats * dose.amount
+      @recipe_prot += dose.ingredient.protein * dose.amount
+      @recipe_salt += dose.ingredient.salt * dose.amount
+    end
+  end
+  # Check how to refactor this code
 end
