b2e9d7a0a4f1862bf41b45f1aa8fadd1ef09cf5c
Joana Bertoldi <joanasoares_1994@msn.com>
2020-12-11 13:52:29 +0000

    Add style to doses_new

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,6 @@ require('turbolinks').start()
 require('@rails/activestorage').start()
 require('channels')
 
-import 'bootstrap';
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -16,6 +14,10 @@ import 'bootstrap';
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
+// External imports
+import 'bootstrap';
+
+// Internal imports
 import { navbar } from '../components/navbar';
 import { toggleForms } from '../components/toggleFormsDisplay';
 
