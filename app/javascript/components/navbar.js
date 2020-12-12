2cc454e0fc1ee732824e19c3bca66307b920b8f6
Joana Bertoldi <joanasoares_1994@msn.com>
2020-12-12 23:16:47 +0000

    Navbar_v2 style first draft. Apply responsiveness for small and medium screens

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -3,9 +3,9 @@ import $ from 'jquery';
 const navbar = () => {
   $('.navTrigger').click(function () {
     $(this).toggleClass('active');
-    console.log("Clicked menu");
-    $("#mainListDiv").toggleClass("show_list");
-    $("#mainListDiv").fadeIn();
+    console.log('Clicked menu');
+    $('#mainListDiv').toggleClass('show_list');
+    $('#mainListDiv').fadeIn();
   });
 }
 
