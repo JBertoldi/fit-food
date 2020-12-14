f32718d2038728cf440746205354545f3616825a
Joana Bertoldi <joanasoares_1994@msn.com>
2020-12-14 23:04:58 +0000

    Add method to increment and decrement recipe's macronutrients upon dose save and destroy

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -4,4 +4,37 @@ class Dose < ApplicationRecord
 
   validates :amount, numericality: { greater_than: 0, message: 'Please enter a number greater than 0.' }
   validates :description, length: { maximum: 50, message: 'Please write a description with up to 50 characters' }
+
+  after_save :increment_recipe_macros!
+  before_destroy :decrement_recipe_macros!
+
+  private
+
+  def calc(macro)
+    self.amount * macro
+  end
+
+  def decrement_recipe_macros!
+    recipe = self.recipe
+    ing = self.ingredient
+    recipe.kcal -= calc(ing.kcal)
+    recipe.carbs -= calc(ing.carbs)
+    recipe.total_fats -= calc(ing.total_fats)
+    recipe.saturated_fats -= calc(ing.saturated_fats)
+    recipe.protein -= calc(ing.protein)
+    recipe.salt -= calc(ing.salt)
+    recipe.save
+  end
+
+  def increment_recipe_macros!
+    recipe = self.recipe
+    ing = self.ingredient
+    recipe.kcal += calc(ing.kcal)
+    recipe.carbs += calc(ing.carbs)
+    recipe.total_fats += calc(ing.total_fats)
+    recipe.saturated_fats += calc(ing.saturated_fats)
+    recipe.protein += calc(ing.protein)
+    recipe.salt += calc(ing.salt)
+    recipe.save
+  end
 end
