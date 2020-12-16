98662b1e995e42ea439d4c564b8c920e9dd6f2d6
Joana Bertoldi <joanasoares_1994@msn.com>
2020-12-16 16:20:10 +0000

    Update model error messages and validations

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,10 +1,10 @@
 class Ingredient < ApplicationRecord
   has_many :doses
 
-  validates :name, uniqueness: { case_sensitive: false, message: 'Ingredient already exists.' }
-  validates :name, length: { in: 3..50, message: 'Please enter a name with a minimum of 3, and a maximum of 50 characters.' }
-  validates :kcal, numericality: { only_integer: true, less_than_or_equal_to: 1_000, message: 'Please enter a decimal number up to 1000.' }
-  validates :carbs, :total_fat, :saturated_fat, :protein, :salt, numericality: { only_integer: true, less_than_or_equal_to: 100, message: 'Please enter a decimal number up to 100.' }
+  validates :name, uniqueness: { case_sensitive: false }
+  validates :name, length: { in: 3..50 }
+  validates :kcal, numericality: { less_than_or_equal_to: 1_000 }
+  validates :carbs, :total_fat, :saturated_fat, :protein, :salt, numericality: { less_than_or_equal_to: 100 }
 
   scope :ordered, -> { order(name: :asc) }
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,8 +3,16 @@ class Recipe < ApplicationRecord
   has_many :doses, dependent: :destroy
   belongs_to :user
 
-  validates :name, format: { with: /\A([a-zA-Z0-9[-']])+([\s][a-zA-Z0-9[-']]+)*\Z/, message: 'The name may only contain letters, numbers, apostrophes, hyphens and single spaces.' }
-  validates :name, length: { in: 5..50, message: 'Please enter a name with a minimum of 5, and a maximum of 50 characters.' }
-  validates :instructions, length: { minimum: 10, message: 'Please write instructions with a minimum of 10 characters.' }, allow_nil: true
-  validates :user, presence: true
+  validates :name, format: { with: /\A([\p{L}0-9[-']])+(\s[\p{L}0-9[-']]+)*\Z/ }
+  validates :name, length: { in: 5..50 }
+  validates :instructions, length: { minimum: 10 }, allow_nil: true
+
+  before_validation :format_details
+
+  private
+
+  def format_details
+    self.name = name.strip.capitalize
+    self.instructions = instructions.strip.capitalize unless instructions.nil?
+  end
 end
