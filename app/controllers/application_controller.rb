a160e49575193ff608730e86826e59316b7a3127
Joana Bertoldi <joanasoares_1994@msn.com>
2020-12-28 23:52:42 +0000

    Add photo attachment to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[username photo])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[username photo])
   end
 end
