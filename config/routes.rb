16648365967e8f9559d330ebdf9438a8fb41c6cd
Joana Bertoldi <joanasoares_1994@msn.com>
2020-12-31 19:25:58 +0000

    Add error messages basic config, routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,15 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   devise_for :users
-  get 'profile', to: 'pages#profile'
+  get '/profile', to: 'pages#profile'
 
   resources :recipes, except: :new do
     resources :doses, only: %i[new create destroy]
   end
 
   resources :ingredients, only: %i[index new create]
+
+  match '/404', to: 'errors#not_found', via: :all
+  match '/422', to: 'errors#unacceptable', via: :all
+  match '/500', to: 'errors#internal_error', via: :all
 end
