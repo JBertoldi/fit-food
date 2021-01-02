7fb0d516364cc6a345d0d539724d51b62b4ce212
Joana Bertoldi <joanasoares_1994@msn.com>
2021-01-02 23:36:14 +0000

    Add lockable and confirmable columns to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :lockable, :confirmable
 
-  has_many :recipes
+  has_many :recipes, dependent: :destroy
 
   has_one_attached :photo
 
