5bdee907c4ff3ce737b8f2626ac66dd86f85c8e1
Joana Bertoldi <joanasoares_1994@msn.com>
2021-01-05 11:47:33 +0000

    Basic config for authentication with username or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_writer :login
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :lockable, :confirmable
@@ -8,4 +10,34 @@ class User < ApplicationRecord
   has_one_attached :photo
 
   validates :username, uniqueness: true
+
+  # Igual à doc
+  # validates :username, uniqueness: { case_sensitive: false }
+
+  # Regex ligeiramente diferente do que eles propunham porque o deles não aceitava á, etc.
+  validates :username, format: { with: /\A[[:alpha:]]*\z/ }
+
+  def login
+    @login || username || email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if (login = conditions.delete(:login))
+      where(conditions.to_h).where(['username = :value OR email = :value', { value: login }]).first
+    elsif conditions.key?(:username) || conditions.key?(:email)
+      where(conditions.to_h).first
+    end
+  end
+
+  # Igual à doc - mas também não consegui assim
+
+  # def self.find_for_database_authentication(warden_conditions)
+  #   conditions = warden_conditions.dup
+  #   if login = conditions.delete(:login)
+  #     where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+  #   elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+  #     where(conditions.to_h).first
+  #   end
+  # end
 end
