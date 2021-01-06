f57c1fb00620a115c92108bbf52601d2022a22e8
Joana Bertoldi <joanasoares_1994@msn.com>
2021-01-06 09:11:01 +0000

    Method was working after all, previously created users didn't have the confirmed_at column value, thus the users were not authorized to login

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,11 +10,6 @@ class User < ApplicationRecord
   has_one_attached :photo
 
   validates :username, uniqueness: true
-
-  # Igual à doc
-  # validates :username, uniqueness: { case_sensitive: false }
-
-  # Regex ligeiramente diferente do que eles propunham porque o deles não aceitava á, etc.
   validates :username, format: { with: /\A[[:alpha:]]*\z/ }
 
   def login
@@ -29,15 +24,4 @@ class User < ApplicationRecord
       where(conditions.to_h).first
     end
   end
-
-  # Igual à doc - mas também não consegui assim
-
-  # def self.find_for_database_authentication(warden_conditions)
-  #   conditions = warden_conditions.dup
-  #   if login = conditions.delete(:login)
-  #     where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-  #   elsif conditions.has_key?(:username) || conditions.has_key?(:email)
-  #     where(conditions.to_h).first
-  #   end
-  # end
 end
