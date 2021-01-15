e0ffa1c5e0faceb5c63d1a11e271721e6d6db5d2
Joana Bertoldi <joanasoares_1994@msn.com>
2021-01-15 19:17:59 +0000

    Add published boolean to recipes.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_14_222931) do
+ActiveRecord::Schema.define(version: 2021_01_15_021431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,6 +85,7 @@ ActiveRecord::Schema.define(version: 2021_01_14_222931) do
     t.integer "cooking_time", default: 0
     t.integer "total_time", default: 0
     t.integer "favourites_count", default: 0
+    t.boolean "published", default: false
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
