edc963b7f1408b191596bb9332e8aba0cd2a7f8d
Joana Bertoldi <joanasoares_1994@msn.com>
2021-01-23 23:54:32 +0000

    Add delete link to recipe show. Style changes

--- app/helpers/recipe_helper.rb
+++ app/helpers/recipe_helper.rb
@@ -1,9 +1,13 @@
 module RecipeHelper
   def author?
-    current_user.id == @recipe.user_id
+    current_user.id == @recipe.user_id unless @recipe.nil?
   end
 
   def favourited?
     current_user.favourited?(@recipe)
+  end
+
+  def edit_ico
+    "<i class='far fa-edit'></i>".html_safe
   end
 end
